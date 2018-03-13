9fa436bf75549c1d2844c80a5f078b8d46af194b
Jas502n <jas502n@gmail.com>
2018-03-13 17:27:07 +0800

    update

---
+++ 03Python变量和数据类型/3-1-Python中数据类型/task.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+### task01
+### 1. 计算十进制整数 45678 和十六进制整数 0x12fd2 之和。
+
+print 45678 + 0x12fd2
+
+### task02
+### 2.请用字符串表示出Learn Python in imooc
+
+print "Learn Python in imooc"
+
+### task03
+### 3. 请计算以下表达式的布尔值（注意==表示判断是否相等）：
+
+print 100 < 99
+
+print 0xff == 255
