16c06b2a28798ce548b4ba288c4950707761cbda
Jas502n <jas502n@gmail.com>
2018-03-13 22:00:41 +0800

    update

---
+++ 03Python变量和数据类型/3-4-Python中什么是变量/task.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+### task01
+### 请将下面两行内容用Python的字符串表示并打印出来：
+### 　　Python was started in 1989 by "Guido".
+### 　　Python is free and easy to learn.
+
+print 'Python was started in 1989 by "Guido".'
+print "Python was started in 1989 by \"Guido\"."
+
+print 'Python is free and easy to learn.'
+print "Python is free and easy to learn."
